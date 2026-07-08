cf9ddbcfc178ad67ca58955f4bc2a126e2d53759
Karl Doherty <100422436+k4rldoherty@users.noreply.github.com>
2026-07-08 15:21:26 +0100

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        int r = 0;
+        int l = 0;
+        int res = 0;
+        int mostOccurringLetter = 0;
+        Dictionary<char, int> d = new();
+
+        while(r < s.Length) {
+            if(!d.TryAdd(s[r], 1)) {
+                d[s[r]]++;
+            }
+            
+            if(d[s[r]] > mostOccurringLetter) mostOccurringLetter = d[s[r]];
+
+            // check window validity
+            var changesToMakeRepeating = (r - l + 1) - mostOccurringLetter;
+
+            if(changesToMakeRepeating <= k) {
+                res = (r - l + 1) > res ? (r - l + 1) : res;
+                // increase r
+                r++;
+            } else {
+                // shrink window until valid again
+                while(changesToMakeRepeating > k) {
+                    d[s[l]]--;
+                    // Note: mostOccurringLetter does not need to be decreased for the algorithm to work
+                    l++;
+                    changesToMakeRepeating = (r - l + 1) - mostOccurringLetter;
+                }
+                r++;
+            }
+        }
+
+        return res;
+    }
+}
